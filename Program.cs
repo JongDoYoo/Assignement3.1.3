78fcd511c2f9afb658608e883ac0cd3218fadd4a
JongD <JongD@JONGDO>
2024-05-21 06:11:37 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Assignement3._1._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a sentence"); //Hi my name is Yoo
+            string input = Console.ReadLine();
+            Console.WriteLine("This sentence has " + NumOfSpaces(input) + (" spaces"));
+
+        }
+        static int NumOfSpaces(string input)
+        {
+            int space = 0;
+            foreach (char letter in input)
+            {
+                if (letter == ' ')
+                {
+                    space++;
+                }                             
+            }
+            return space;
+
+
+        }
+}
+
+    }
